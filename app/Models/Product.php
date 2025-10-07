fb8c5122267e61866aba9414868d074a5a680cd7
molengeek <saoussan.elk@gmail.com>
2025-10-07 13:00:37 +0200

    prix + promos index OK

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -23,6 +23,7 @@ class Product extends Model
         'img_3', 
         'img_4',
     ];
+    protected $appends = ['price_promo'];
 
     public function category()
     {
@@ -106,5 +107,17 @@ class Product extends Model
         
         $cartItem = $this->cartItems()->where('user_id', auth()->id())->first();
         return $cartItem ? $cartItem->quantity : 0;
+    }
+    /**
+ * Calculer le prix après promotion
+ */
+    public function getPricePromoAttribute()
+    {
+        if (!$this->promo) {
+            return null;
+        }
+        
+        $discount = ($this->price * $this->promo->percent) / 100;
+        return number_format($this->price - $discount, 2, '.', '');
     }
 }
